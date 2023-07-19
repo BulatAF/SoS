179c8cda916828f643c68b6acbf179e083ba5cf5
DroWiShko <138319088+DroWiShko@users.noreply.github.com>
2023-07-19 17:18:33 +0300

    19.07 (17:18)
    Звук почти доделан

--- Assets/Code/ZvukBany.cs
+++ Assets/Code/ZvukBany.cs
@@ -11,25 +11,63 @@ public class ZvukBany : MonoBehaviour
 	[SerializeField] private AudioClip[] ban5;
 	private AudioSource aud;
 	private bool one;//чтобы один раз	
-	private int repl;//чтобы записывать предыдущюю replwhat2
+	public  int repl;//чтобы записывать предыдущюю replwhat2
+	public int Repl2;
     void Start()
-    {/*
-		aud = GetComponent<AudioSource>().clip;
-		aud = ban1[Monolog.replWhat2];
-		StartCoroutine(Shit());*/
+    {
+		aud = GetComponent<AudioSource>();
+		aud.clip = ban1[Monolog.replWhat2];
+		StartCoroutine(Shit());
     }
 		IEnumerator Shit()// карутина чтобы проигрывало
 		{
-			gameObject.GetComponent<AudioSource>().Play();//играть
+			aud.Play();//играть
 			yield return  new WaitForSeconds(1000000f);//ждать
-			gameObject.GetComponent<AudioSource>().Stop();//не играть
+			aud.Stop();//не играть
 		}
 		
     void Update()
     {
-		if(Monolog.whatDialog2 == 1)
+		Repl2 = Monolog.replWhat2;
+		if(Monolog.whatDialog2 == 1)//начальный монолог
 				{
-					gameObject.GetComponent<AudioSource>().clip = ban1[Monolog.replWhat2];
+					aud.clip= ban1[Monolog.replWhat2];
+					if(repl != Monolog.replWhat2)
+					{
+						StartCoroutine(Shit());
+						repl = Monolog.replWhat2;
+					}
+				}
+		if(Monolog.whatDialog2 == -1)//монолог влево 
+				{
+					aud.clip= ban2[target.TfalsE];
+					if(repl != target.TfalsE)
+					{
+						StartCoroutine(Shit());
+						repl = target.TfalsE;
+					}
+				}
+		if(Monolog.whatDialog2 == 3 && target.TtruE == 1)// монолог бл бой
+				{
+					aud.clip= ban3[Monolog.replWhat2];
+					if(repl != Monolog.replWhat2)
+					{
+						StartCoroutine(Shit());
+						repl = Monolog.replWhat2;
+					}
+				}
+		if(Monolog.whatDialog2 == 4 && target.TtruE == 2)//монолог дал бой
+				{
+					aud.clip= ban4[Monolog.replWhat2];
+					if(repl != Monolog.replWhat2)
+					{
+						StartCoroutine(Shit());
+						repl = Monolog.replWhat2;
+					}
+				}
+		if(Monolog.whatDialog2 == 5 && target.TtruE == 3)// монолог итог
+				{
+					aud.clip= ban5[Monolog.replWhat2];
 					if(repl != Monolog.replWhat2)
 					{
 						StartCoroutine(Shit());
